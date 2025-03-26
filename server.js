91e06d7366db870167eeb1d538f52e0869a74ca9
spywar18 <singh.shikhar0712@gmail.com>
2025-03-26 19:15:51 +0530

    handling cors

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: 'http://localhost:5173, ',
     allowedHeaders: ['Content-Type', 'Authorization'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],  
     credentials: true  
